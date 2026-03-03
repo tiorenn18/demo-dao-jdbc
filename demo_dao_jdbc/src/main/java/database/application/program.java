51390c7c4d48634feb510d78a34adb1d9f7745d8
Tiorenn <ericorenanleal124@gmail.com>
2026-03-03 15:47:01 -0300

    Testando classe seller

--- demo_dao_jdbc/src/main/java/database/application/program.java
+++ demo_dao_jdbc/src/main/java/database/application/program.java
@@ -1,10 +1,17 @@
 package database.application;
 
+import java.util.Date;
+
 import database.classes.Department;
+import database.classes.Seller;
 
 public class program {
     public static void main(String[] args) {
         Department obj = new Department(1, "books");
         System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 }
