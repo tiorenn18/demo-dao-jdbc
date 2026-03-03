959f548adc532d9a91076d1b5efac0eb4d4a821d
Tiorenn <ericorenanleal124@gmail.com>
2026-03-03 15:23:37 -0300

    Teste da classe departament

--- demo_dao_jdbc/src/main/java/database/application/program.java
+++ demo_dao_jdbc/src/main/java/database/application/program.java
@@ -1,5 +1,10 @@
 package database.application;
 
+import database.classes.Department;
+
 public class program {
-    
+    public static void main(String[] args) {
+        Department obj = new Department(1, "books");
+        System.out.println(obj);
+    }
 }
