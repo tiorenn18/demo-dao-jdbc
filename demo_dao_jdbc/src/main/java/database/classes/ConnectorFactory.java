f48e50d97eede6cf17185ca6d8395b7c72ead1d0
Tiorenn <ericorenanleal124@gmail.com>
2026-03-03 14:48:36 -0300

    Creating project and git repository

---
+++ demo_dao_jdbc/src/main/java/database/classes/ConnectorFactory.java
@@ -0,0 +1,16 @@
+package database.classes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectorFactory {
+    
+    private static final String URL =  "jdbc:mysql://localhost:3306/coursedb";
+    private static final String USER = "developer";
+    private static final String USER_PASSWORD = "12345";
+
+    public Connection getConnection() throws SQLException { 
+        return DriverManager.getConnection(URL,USER,USER_PASSWORD);
+    }
+}
